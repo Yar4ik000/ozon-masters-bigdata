f8abef61503cbd3839d37e774e00b16a01fcd7c9
Yar4ik000 <Yari2012-Yarik@mail.ru>
2022-03-07 13:38:12 +0000

    add filter_cond

---
+++ projects/1/filter_cond.py
@@ -0,0 +1,13 @@
+#
+#
+def filter_cond(line_dict):
+    """Filter function
+    Takes a dict with field names as argument
+    Returns True if conditions are satisfied
+    """
+    if not line_dict['I1']:
+        return False
+    cond_match = (
+       int(line_dict["I1"]) > 20 and int(line_dict['I1']) < 40
+    ) 
+    return True if cond_match else False
