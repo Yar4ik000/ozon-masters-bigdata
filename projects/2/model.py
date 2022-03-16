a9f83090570140d1b61ba0d8a077f1a4c3e85c35
Yar4ik000 <Yari2012-Yarik@mail.ru>
2022-03-16 14:09:53 +0000

    add model

--- projects/2/model.py
+++ projects/2/model.py
@@ -1,1 +1,36 @@
+#!/opt/conda/envs/dsenv/bin/python
+
+
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler, OneHotEncoder
+from sklearn.linear_model import LogisticRegression
+from sklearn.model_selection import train_test_split
+from sklearn.impute import SimpleImputer
+from sklearn.compose import ColumnTransformer
+
+
+numeric_features = ["if"+str(i) for i in range(1,14)]
+categorical_features = ["C"+str(i) for i in range(1, 27)] + ['day_number']
+
+fields = ['id', "label"] + numeric_features
+
+numeric_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='median')),
+    ('scaler', StandardScaler())
+])
+
+categorical_transformer = Pipeline(steps=[
+    ('imputer', SimpleImputer(strategy='constant', fill_value='missing'))
+])
+
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('num', numeric_transformer, numeric_features)
+    ]
+)
+
+model = Pipeline(steps=[
+    ('preprocessor', preprocessor),
+    ('logisticregression', LogisticRegression())
+])
 
